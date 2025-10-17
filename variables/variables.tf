be4a1903b1cf4f6222763f7aa4e69dd91616dba3
smily kancharla <smilykancharla@gmail.com>
2025-10-17 11:00:59 +0530

    new

---
+++ variables/variables.tf
@@ -0,0 +1,32 @@
+variable  "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+}
+
+variable  "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "ec2-tags" {
+     type = map
+      default = {
+        Name = "terraform"
+        Terraform = "true"
+    }
+}
+
+variable "cidir" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
+
+variable "protocol" {
+    type = string
+    default = "-1"
+}
+
+variable "sg_id" {
+    type = string
+    default = "all_allow"
+}
